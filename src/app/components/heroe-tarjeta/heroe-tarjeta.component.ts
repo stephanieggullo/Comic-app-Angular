32f4bbd7d6b450cb21ae1dbe7bd911a2c67affb7
Stephanie Guevara <stephanie.estela.guevara.gullo@everis.com>
2019-03-29 14:09:26 +0100

    router

---
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-heroe-tarjeta',
+  templateUrl: './heroe-tarjeta.component.html',
+  styleUrls: ['./heroe-tarjeta.component.css']
+})
+export class HeroeTarjetaComponent implements OnInit {
+
+  @Input() heroe: any = {};
+  @Input() index: number;
+
+  constructor( private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  HeroeDetail() {
+    console.log(this.index);
+    this.router.navigate(['/heroe', this.index]);
+
+  }
+
+}
